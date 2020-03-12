7150c944df794a9b947d78b16c0acc52c669e816
rungnapasu <rungnapa.su.61@ubu.ac.th>
2020-03-12 17:40:28 +0700

    final

---
+++ chatgroup/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from joblib import load
+from sklearn.datasets import fetch_20newsgroups
+
+data = fetch_20newsgroups()
+categories = ['comp.graphics', 'rec.autos', 'rec.motorcycles']
+train = fetch_20newsgroups(subset='train', categories=categories)
+
+# Create your views here.
+def index(req):
+    model = load('./chatgroup/static/chatgroup.model')
+    label = ""
+    chat  = ""
+    chat  = ""
+    if req.method == 'POST':
+        print("POST IN")
+        chat = str(req.POST['chat'])
+        print(chat)
+        pred = model.predict([chat])
+        label = train.target_names[pred[0]]
+    return render(req, 'chatgroup/index.html' ,{
+            'label':label,
+    })
